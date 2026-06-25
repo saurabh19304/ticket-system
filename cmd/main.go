2a6a9fa1c92c99ec84363ad066169c45660df156
saurabhtiwari-e-sutra <saurabhtiwari@e-sutra.com>
2026-06-25 18:39:16 +0530

    setup the go project structure and health check endpoint

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"ticket-system/internal/database"
+	"ticket-system/internal/routes"
+
+)
+
+func main() {
+	database.ConnectDatabase()
+
+ router := gin.Default()
+
+ routes.RegisterRoutes(router)
+
+ router.Run(":8080")
+}
---
+++ internal/routes/routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	
+	"ticket-system/internal/handlers"
+)
+
+func RegisterRoutes(router *gin.Engine) {
+	router.GET("/health", handlers.HealthCheck)
+}
